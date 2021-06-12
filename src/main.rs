a9e7c935d2ca58d0ada828ac874a03103db0a24e
paulstey <paulstey@gmail.com>
2021-06-12 18:21:26 -0400

    get first test working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+//! src/main.rs
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port.");
+
+    let port = listener.local_addr().unwrap().port();
+
+    println!("Server is now listening on http://127.0.0.1:{}", port);
+    
+    run(listener)?.await 
 }
