2efbfafeb76f20acdf3c16c72629b7be56d7bb1c
paulstey <paulstey@gmail.com>
2021-08-28 13:46:15 -0400

    starting chpt 4

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@
 use crate::routes::{health_check::health_check, subscriptions::subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -15,6 +16,8 @@ pub fn run(
 
     let server = HttpServer::new(move || {
         App::new()
+            // Middlewares are added using the `wrap` methond on `App`
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Register the connection as part of the application state
