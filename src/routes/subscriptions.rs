05d55cfdfbd48a379d6d4bc8b7e741c3d1a12d3d
paulstey <paulstey@gmail.com>
2021-10-23 18:26:49 -0400

    feat: get span working correctly

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,7 @@ use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
+use tracing;
 
 
 #[derive(serde::Deserialize)]
@@ -17,6 +18,22 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    // Let's generate a random unique identifier 
+    let request_id = Uuid::new_v4();
+
+    // Spans, like logs, have an associated level 
+    // `info_span` creates a span at the info-level
+    // NOTE: the % symbol tells `tracing` crate to use Display 
+    // implementation for logging purpose
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,        
+        subscriber_email = %form.email,
+        subscriber_name = %form.name 
+    );
+
+    let _request_span_guard = request_span.enter();
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -30,9 +47,19 @@ pub async fn subscribe(
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!(
+                "request_id {} - Saving new subscriber details in the database",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            print!("Failed to execute query {}", e);
+            tracing::error!(
+                "request_id {} - Failed to execute query: {:?}", 
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
