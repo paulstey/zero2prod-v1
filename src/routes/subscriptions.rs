5cc2165a9938c99c00bb8a24dde185a7f8da0230
paulstey <paulstey@gmail.com>
2021-11-24 13:43:25 -0500

    wip: just now starting chpt 6.3

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -5,6 +5,7 @@ use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
 use tracing;
+use unicode_segmentation::UnicodeSegmentation;
 
 
 #[derive(serde::Deserialize)]
@@ -25,6 +26,9 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest::finish();
+    }
     match insert_subscriber(&pool, &form).await
     {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -58,4 +62,18 @@ pub async fn insert_subscriber(
         e 
     })?;       // Using `?` to return early if function failed
     Ok(())
+}
+
+
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+
+    let is_too_long = s.grapheme(true).count() > 256;
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+
+    // return `false` if any of our checks is violated
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
